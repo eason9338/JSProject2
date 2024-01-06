bab919f38996cb5e9eefc1a3104163cdb1c11530
eason <9338akira@gmail.com>
2024-01-06 12:57:30 +0800

    Update p5playlab.js

--- public/p5playlab.js
+++ public/p5playlab.js
@@ -18,53 +18,53 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(800, 600);
-    // Step 5: set the velocity for the mountain images
-    mountains[0].setVelocity(-0.6, 0);
-    mountains[1].setVelocity(-0.1, 0);
-    mountains[2].setVelocity(-1.4, 0);
-    mountains[3].setVelocity(-1.8, 0);
+    createCanvas(1250, 691);
+    // // Step 5: set the velocity for the mountain images
+    // mountains[0].setVelocity(-0.6, 0);
+    // mountains[1].setVelocity(-0.1, 0);
+    // mountains[2].setVelocity(-1.4, 0);
+    // mountains[3].setVelocity(-1.8, 0);
 }
 
 function draw() {
-    background(249);
-    // Step 3: add keyboard commands to control the airplane
-    if(keyIsDown(UP_ARROW)) {
-        plane.setVelocity(0, -2);
-    }else if(keyIsDown(DOWN_ARROW)) {
-        plane.setVelocity(0, 2);
-    }else if(keyIsDown(LEFT_ARROW)) {
-        plane.setVelocity(-2, 0);
-    }else if(keyIsDown(RIGHT_ARROW)) {
-        plane.setVelocity(2, 0);
-    }else {
-        plane.setVelocity(0, 0);
-    }
+    background(120);
+    // // Step 3: add keyboard commands to control the airplane
+    // if(keyIsDown(UP_ARROW)) {
+    //     plane.setVelocity(0, -2);
+    // }else if(keyIsDown(DOWN_ARROW)) {
+    //     plane.setVelocity(0, 2);
+    // }else if(keyIsDown(LEFT_ARROW)) {
+    //     plane.setVelocity(-2, 0);
+    // }else if(keyIsDown(RIGHT_ARROW)) {
+    //     plane.setVelocity(2, 0);
+    // }else {
+    //     plane.setVelocity(0, 0);
+    // }
 
 
-    for (let i = 0;i < mountains.length;i++) {
-        // Step 5: add code here to detect (add adjust) the mountains positions
-        if(mountains[i].position.x <= 0) {
-            mountains[i].position.x = 800;
-        } 
+    // for (let i = 0;i < mountains.length;i++) {
+    //     // Step 5: add code here to detect (add adjust) the mountains positions
+    //     if(mountains[i].position.x <= 0) {
+    //         mountains[i].position.x = 800;
+    //     } 
         
-        // Step 6: add code here to detect the collision between plane and the mountains and to change the image if a collision is detected
-        if (mountains[i].overlap(plane)) {
-            plane.changeImage('explode');
-            break;
-        } else {
-            plane.changeImage('plane');
-        }
+    //     // Step 6: add code here to detect the collision between plane and the mountains and to change the image if a collision is detected
+    //     if (mountains[i].overlap(plane)) {
+    //         plane.changeImage('explode');
+    //         break;
+    //     } else {
+    //         plane.changeImage('plane');
+    //     }
        
 
-    }
+    // }
 
     drawSprites();
 }
 
 function mouseClicked() {
-    // Step 4: add code here to fire the machine gun
-    let s = createSprite(plane.position.x, plane.position.y, 10, 10);
-    s.setVelocity(3, 0);
-    s.life = 150;
+    // // Step 4: add code here to fire the machine gun
+    // let s = createSprite(plane.position.x, plane.position.y, 10, 10);
+    // s.setVelocity(3, 0);
+    // s.life = 150;
 }
