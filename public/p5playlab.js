0a790dba3924c0d90f45c5cec83dec27f7839a79
eason <9338akira@gmail.com>
2024-01-06 14:53:32 +0800

    l

--- public/p5playlab.js
+++ public/p5playlab.js
@@ -1,4 +1,7 @@
 let roads;
+let scooter;
+let width = 1250;
+let height = 690;
 
 function preload() {
    
@@ -7,23 +10,31 @@ function preload() {
         scaledImages.push(loadAndScaleImage('images/Gaming/roads/road' + i + '.png', 0.18));
     }
     roads = loadAnimation(scaledImages[0], scaledImages[1], scaledImages[2], scaledImages[3]);
+
+    //imageMode(CENTER);
+    scooter = createSprite(width / 2, height - 200);
+    scooter.addImage(loadAndScaleImage('images/BeginScene/Scooter.png', 0.18));
 }
 
 function setup() {
-    createCanvas(1250, 691);
+    createCanvas(width, height);
 
 }
 
 function draw() {
     background(120);
-        let centerX = width / 2;
-    let centerY = height / 2 + 100;
+    let roadX = width / 2;
+    let roadY = height / 2 + 100;
     drawSprites();
-    animation(roads, centerX, centerY);
+    animation(roads, roadX, roadY);
+
+    drawSprites();
 }
 
 function mouseClicked() {
-    
+    if(keyIsDown(LEFT_ARROW)){
+        
+    }
 }
 
 function loadAndScaleImage(path, scale) {
